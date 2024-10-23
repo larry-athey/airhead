9312f62427c5359c3ce8c2b3a382e7eaff1ab4c5
Larry Athey <panhandleponics@larryathey.com>
2024-10-23 09:38:26 -0600

    Update the_brains.ino

--- the_brains/the_brains.ino
+++ the_brains/the_brains.ino
@@ -5,6 +5,10 @@
 // your Arduino IDE to upload to that board. See: https://github.com/Xinyuan-LilyGO/T-Display-S3
 // Make sure that you only install the Espressif ESP32 v2.0.14 board library since the 3.x stuff
 // is not backward compatible and will throw syntax errors all over the place. (Nice fkin upgrade)
+//
+// This code is written for a 320x170 touch screen. If you're wanting to use a different ESP32 and
+// a larger screen, you'll need to modify a lot of coordinate values in order to use more than the
+// upper left corner of the screen.
 //------------------------------------------------------------------------------------------------
 #include "Arduino_GFX_Library.h" // Standard GFX library for Arduino, built with version 1.4.9
 #include "FreeSans10pt7b.h"      // https://github.com/moononournation/ArduinoFreeFontFile.git 
@@ -15,6 +19,8 @@
 #define ONE_WIRE 1               // 1-Wire network pin for DS18B20 temperature sensor
 #define PIN_LCD_BL 38            // Screen backlight pin
 #define PIN_POWER_ON 15          // Screen on/off pin
+#define INC_BTN 0                // Value + button
+#define DEC_BTN 14               // Value - button
 //------------------------------------------------------------------------------------------------
 Arduino_DataBus *bus = new Arduino_ESP32LCD8(7 /* DC */, 6 /* CS */, 8 /* WR */, 9 /* RD */, 39 /* D0 */, 40 /* D1 */, 41 /* D2 */, 42 /* D3 */,
                                              45 /* D4 */, 46 /* D5 */, 47 /* D6 */, 48 /* D7 */);
@@ -26,7 +32,11 @@ DallasTemperature DT(&oneWire);
 Preferences preferences;
 //------------------------------------------------------------------------------------------------
 void setup() {
+  Serial.begin(9600);
+  while (! Serial) delay(10);
+  Serial.println("");
   DT.begin();
+
   pinMode(PIN_POWER_ON,OUTPUT);
   digitalWrite(PIN_POWER_ON,HIGH);
   ledcSetup(0,2000,8);
@@ -41,6 +51,16 @@ void setup() {
 //------------------------------------------------------------------------------------------------
 void loop() {
 
+  if (digitalRead(INC_BTN) == 0) {
+    Serial.println("+ button pressed");
+    while (digitalRead(INC_BTN) == 0) delay(10);
+  }
 
+  if (digitalRead(DEC_BTN) == 0) {
+    Serial.println("- button pressed");
+    while (digitalRead(DEC_BTN) == 0) delay(10);
+  }
+
+  delay(100);
 }
 //------------------------------------------------------------------------------------------------
