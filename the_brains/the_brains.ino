cca1bf6687134996e263534b381ff9f0f02c3dbf
Larry Athey <panhandleponics@larryathey.com>
2024-10-30 14:27:29 -0600

    Update the_brains.ino

--- the_brains/the_brains.ino
+++ the_brains/the_brains.ino
@@ -80,11 +80,11 @@ int TimeX1 = 0, TimeY1 = 0, TimeX2 = 0, TimeY2 = 0;
 #define STOPBTN RGB565(230,0,0)
 #define TEMPBTN RGB565(0,210,210)
 #define PWRBTN RGB565(50,50,230)
-#define HILITE RGB565(230,230,230)
-#define TEXT RGB565(245,245,245)
 #define STARTBTN RGB565(0,210,210)
 #define ENDBTN RGB565(0,210,210)
 #define TIMEBTN RGB565(50,50,230)
+#define HILITE RGB565(230,230,230)
+#define TEXT RGB565(245,245,245)
 //------------------------------------------------------------------------------------------------
 Arduino_DataBus *bus = new Arduino_ESP32PAR8Q(7 /* DC */, 6 /* CS */, 8 /* WR */, 9 /* RD */,39 /* D0 */, 40 /* D1 */, 41 /* D2 */, 42 /* D3 */, 45 /* D4 */, 46 /* D5 */, 47 /* D6 */, 48 /* D7 */);
 Arduino_GFX *gfx = new Arduino_ST7789(bus, 5 /* RST */, 0 /* rotation */, true /* IPS */, 170 /* width */, 320 /* height */, 35 /* col offset 1 */, 0 /* row offset 1 */, 35 /* col offset 2 */, 0 /* row offset 2 */);
